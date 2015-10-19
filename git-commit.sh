e426c957f31fa0a8a195fbbc93014c66d5c21bdc
PERREYMOND Gilles <gperreymond@gmail.com>
2015-10-19 13:09:46 +0000

    initial commit

--- git-commit.sh
+++ git-commit.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-git add -i
-git commit -m "$2"
-git push origin $1
+# run tests
+npm test
+
+# if tests are not 100% ok, then no need to commit...
+if [ $? -eq 0 ]
+  then
+    git add -i
+    git commit -m "$2"
+    git push origin $1
+fi
