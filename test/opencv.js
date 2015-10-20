3577db5fa301ae4a1545b02d716d0f7313c8f70b
PERREYMOND Gilles <gperreymond@gmail.com>
2015-10-20 13:25:30 +0000

    update gitlabci yml

--- test/opencv.js
+++ test/opencv.js
@@ -7,3 +7,16 @@ chai.use(chaiHttp);
 
 var cloudcv = require('../build/Release/cloudcv');
 
+//console.log(cloudcv.buildInformation());
+
+describe('opencv', function () {
+  it('should return the version of cloudcv we use', function (done) {
+    try {
+      expect(cloudcv.version()).to.be.equal('3.0');
+      done();
+    } catch (e) {
+      done(e);
+    }
+  });
+});
+
